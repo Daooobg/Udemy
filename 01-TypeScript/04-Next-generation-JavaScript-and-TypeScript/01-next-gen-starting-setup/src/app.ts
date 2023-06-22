89b829d4d1c8b85d345e3d54242db046109ccdc9
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-06-22 11:10:53 +0100

    feat: practice

--- 01-TypeScript/04-Next-generation-JavaScript-and-TypeScript/01-next-gen-starting-setup/src/app.ts
+++ 01-TypeScript/04-Next-generation-JavaScript-and-TypeScript/01-next-gen-starting-setup/src/app.ts
@@ -1,1 +1,40 @@
-// Code goes here!
+//  Default arguments needs to be in the end
+const add = (a: number, b: number = 1) => a + b;
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', (event) => console.log(event));
+}
+
+printOutput(add(5));
+
+const hobbies = ['sports', 'cooking'];
+const activeHobbies = ['hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: 'Max',
+  age: 30,
+};
+
+const copiedPerson = { ...person };
+
+const addNumbers = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = addNumbers(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1);
+
+const { name: userName, age } = person;
+console.log(userName);
