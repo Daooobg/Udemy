7bcd55ff171f93c4f4408ababfdee7e6fc5f0ec8
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-06-24 08:52:36 +0100

    feat: render the form

--- 01-TypeScript/09-DragAndDrop-Project/src/app.ts
+++ 01-TypeScript/09-DragAndDrop-Project/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
