407ed8f2687c5b062dca662f238bfbcf61c04066
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-06-30 19:36:13 +0100

    feat: webpack dev and prod

---
+++ 01-TypeScript/11-Webpack-and-TypeScript/src/components/project-item.ts
@@ -0,0 +1,51 @@
+import { Draggable } from '../interfaces/drag-drop';
+import { Component } from './base-component';
+import { Project } from '../models/project';
+import { Autobind } from '../decorators/autobind';
+
+// ProjectItem Class
+
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  @Autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log('dragEnd');
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler);
+    this.element.addEventListener('dragend', this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3')!.textContent =
+      this.persons + ' Persons assigned';
+    this.element.querySelector('p')!.textContent = this.project.description;
+  }
+}
