6309b23c9dece7f579d9037d14b7a227d3331e04
Ivaylo Ivanov <ivaylo_ivanov84@yahoo.co.uk>
2023-06-30 18:32:44 +0100

    feat: ES Modules

---
+++ 01-TypeScript/10-Namespaces-and-Modules/02-ES-Modules/src/models/project.ts
@@ -0,0 +1,16 @@
+// Project Type
+
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
